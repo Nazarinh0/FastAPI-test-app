25138c5a938dc82012d6f391f6efb52c9abdce82
Nazar Annakhalov <nazar.annahalov@yandex.com>
2023-12-26 20:58:00 +0500

    add models and compound structure to main.py

--- test_app/main.py
+++ test_app/main.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 
 app = FastAPI(
@@ -10,6 +14,9 @@ fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "trader", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2023-01-01T12:23:57", "type_degree": "expert"}
+    ]},
 ]
 
 @app.get("/")
@@ -22,7 +29,25 @@ def get_all_users():
     return fake_users
 
 
-@app.get("/users/{user_id}")
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
@@ -43,3 +68,18 @@ def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}

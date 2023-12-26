30d7ea19851369ba370f65c3ca5f0306f5480c28
Nazar Annakhalov <nazar.annahalov@yandex.com>
2023-12-26 18:43:35 +0500

    add content to main.py

--- test_app/main.py
+++ test_app/main.py
@@ -5,6 +5,23 @@ app = FastAPI(
     title="TestApp"
 )
 
+
+fake_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
 @app.get("/")
 def hello():
     return "Hello, World!"
+
+
+@app.get("/users")
+def get_all_users():
+    return fake_users
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]

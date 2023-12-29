ce7c894f5082ac7eb6fdaf713609e03eb68b750e
Nazar Annakhalov <nazar.annahalov@yandex.com>
2023-12-29 22:57:20 +0500

    add auth and register routers to main.py

--- test_app/main.py
+++ test_app/main.py
@@ -6,12 +6,33 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ValidationException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
+from fastapi_users import FastAPIUsers
+from test_app.auth.auth import auth_backend
+from test_app.auth.manager import get_user_manager
+from test_app.auth.schemas import UserCreate, UserRead
+from test_app.models.models import User
 
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 app = FastAPI(
     title="TestApp"
 )
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
 @app.exception_handler(ValidationException)
 async def ValidationException(request: Request, exc: ValidationException):

a98bf7f74b5bae8426e0512ea8b636e2f24747f3
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-01-07 20:44:13 +0500

    create operation model in operations/models.py

---
+++ src/operations/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Table
+from sqlalchemy.sql.schema import MetaData
+
+
+metadata = MetaData()
+
+operation = Table(
+    "operation",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("quantity", String),
+    Column("figi", String),
+    Column("instrument_type", String, nullable=True),
+    Column("date", TIMESTAMP),
+    Column("type", String),
+)

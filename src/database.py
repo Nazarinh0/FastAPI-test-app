dac62cc74588fd57ef01c08682bfc5e2c8222ce0
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-05 22:41:27 +0500

    update database.py

--- src/database.py
+++ src/database.py
@@ -1,16 +1,11 @@
-from datetime import datetime
 from typing import AsyncGenerator
-
 from fastapi import Depends
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy import TIMESTAMP, ForeignKey, Integer, String
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.ext.declarative import DeclarativeMeta
-from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, relationship, DeclarativeBase
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy import TIMESTAMP, Column, ForeignKey, Table
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-
-from src.auth.models import role
 
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
@@ -20,17 +15,8 @@ class Base(DeclarativeBase):
     pass
 
 
-class User(SQLAlchemyBaseUserTable[int], Base):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String, nullable=False)
-    username: Mapped[str] = mapped_column(String, nullable=False)
-    registered_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.utcnow)
-    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('role.id'))
-    role: Mapped["Role"] = relationship("Role", back_populates="users")
-
-
 engine = create_async_engine(DATABASE_URL)
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
@@ -39,4 +25,48 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    from src.auth.models import User
+    
     yield SQLAlchemyUserDatabase(session, User)
+
+
+# class ParentOTM(Base):
+#     __tablename__ = "parent_table"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     children: Mapped[list["ChildOTM"]] = relationship(back_populates="parent")
+
+
+# class ChildOTM(Base):
+#     __tablename__ = "child_table"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     parent_id: Mapped[int] = mapped_column(ForeignKey("parent_table.id"))
+#     parent: Mapped["ParentOTM"] = relationship(back_populates="children")
+
+
+
+# association_table = Table(
+#     "association_table",
+#     Base.metadata,
+#     Column("parent", ForeignKey("parent.id"), primary_key=True),
+#     Column("child", ForeignKey("child.id"), primary_key=True),
+# )
+
+
+# class ParentMTM(Base):
+#     __tablename__ = "parent"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     children: Mapped[list['ChildMTM']] = relationship(
+#         secondary=association_table, back_populates="parents"
+#     )
+
+
+# class ChildMTM(Base):
+#     __tablename__ = "child"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     parents: Mapped[list['ParentMTM']] = relationship(
+#         secondary=association_table, back_populates="children"
+#     )

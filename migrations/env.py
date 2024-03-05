2af7e5f2af2e5b723d2004029500ae6282553ed6
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-05 21:12:18 +0500

    import Base and models for alembic autogenerate

--- migrations/env.py
+++ migrations/env.py
@@ -11,8 +11,9 @@ import sys
 sys.path.append(os.path.join(sys.path[0], "src"))
 
 from src.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-from src.auth.models import metadata as metadata_auth
-from src.operations.models import metadata as metadata_operations
+from src.database import Base
+from src.auth import models
+from src.operations import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -34,7 +35,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata_auth, metadata_operations]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

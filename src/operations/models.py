054f3567e459e5daa964ea499b1a779086d8b249
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-06 22:38:24 +0500

    create Operation model and revision

--- src/operations/models.py
+++ src/operations/models.py
@@ -1,16 +1,16 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP, Table
-from sqlalchemy.sql.schema import MetaData
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, TIMESTAMP
+
+from src.database import Base
 
 
-metadata = MetaData()
+class Operation(Base):
+    __tablename__ = "operation"
 
-operation = Table(
-    "operation",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("quantity", String),
-    Column("figi", String),
-    Column("instrument_type", String, nullable=True),
-    Column("date", TIMESTAMP),
-    Column("type", String),
-)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    quantity: Mapped[str] = mapped_column(String)
+    figi: Mapped[str] = mapped_column(String)
+    instrument_type: Mapped[str | None] = mapped_column(String, nullable=True)
+    date: Mapped[datetime] = mapped_column(TIMESTAMP)
+    type: Mapped[str] = mapped_column(String)

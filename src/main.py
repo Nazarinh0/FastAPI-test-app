cd3dbab73ad6761c4aa2715a844e5fd35e4b5e02
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-07 00:58:11 +0500

    small fixes in main.py

--- src/main.py
+++ src/main.py
@@ -6,11 +6,12 @@ from fastapi_users import FastAPIUsers
 
 from src.auth.auth import auth_backend
 from src.auth.manager import  get_user_manager
-from src.auth.schemas import UserCreate, UserRead, RoleCreate, RoleRead
-from src.auth.models import User, Role
+from src.auth.schemas import UserCreate, UserRead
+from src.auth.models import User
 from src.operations.router import router as router_operation
 from src.auth.router import router as router_auth
 from src.database import get_async_session
+
 
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
@@ -22,17 +23,16 @@ app = FastAPI(title="TestApp")
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth/jwt",
-    tags=["Auth"],
+    tags=["Authentication"],
 )
-
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
-    tags=["Auth"],
+    tags=["Authentication"],
 )
-
 app.include_router(router_operation)
 app.include_router(router_auth)
+
 
 @app.exception_handler(ValidationException)
 async def ValidationException(request: Request, exc: ValidationException):

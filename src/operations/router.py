9acfef4cdbe6f374139b9502f4aea03aa48e51db
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-07 01:50:52 +0500

    add content to router.py

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.operations.models import Operation
+from src.database import get_async_session
+from src.operations.schemas import OperationCreate, OperationRead
 
 
 router = APIRouter(
@@ -7,6 +13,26 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def get_operations():
-    return {"message": "Hello World"}
+@router.get("/", response_model=list[OperationRead])
+async def get_operations(session: AsyncSession = Depends(get_async_session)):
+    query = select(Operation)
+    result = await session.execute(query)
+    operations = result.scalars().all()
+    return operations
+
+
+@router.post("/{operation_id}", response_model=OperationRead)
+async def get_operation(operation_id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(Operation).where(Operation.type == operation_id)
+    result = await session.execute(query)
+    operations = result.scalars().all()
+    return operations
+
+
+@router.post("/", response_model=OperationRead)
+async def add_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    new_operation = Operation(**new_operation.model_dump())
+    session.add(new_operation)
+    await session.flush()
+    await session.commit()
+    return new_operation

fb1a2d13b92874ec0c8ff48834eac272c76f742f
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-06 16:17:12 +0500

    load SECRET from config

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -5,25 +5,23 @@ from fastapi_users import BaseUserManager, IntegerIDMixin, schemas, models, exce
 
 from src.database import get_user_db
 from src.auth.models import User
-
-
-SECRET = "SECRET"
+from src.config import SECRET
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
 
-    async def on_after_register(self, user: User, request: Optional[Request] = None):
+    async def on_after_register(self, user: User, request: Request | None = None):
         print(f"User {user.id} has registered.")
 
     async def on_after_forgot_password(
-        self, user: User, token: str, request: Optional[Request] = None
+        self, user: User, token: str, request: Request | None = None
     ):
         print(f"User {user.id} has forgot their password. Reset token: {token}")
 
     async def on_after_request_verify(
-        self, user: User, token: str, request: Optional[Request] = None
+        self, user: User, token: str, request: Request | None = None
     ):
         print(f"Verification requested for user {user.id}. Verification token: {token}")
 

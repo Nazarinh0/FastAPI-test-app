7968e22b5659b3ba06c5a52eac14b92c587292a2
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-07 00:59:27 +0500

    add content to routers.py - User and Role Create-Read

--- src/auth/router.py
+++ src/auth/router.py
@@ -2,17 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from src.auth.schemas import RoleCreate, RoleRead
+from src.auth.schemas import RoleCreate, RoleRead, UserRead
 from src.database import get_async_session
-from src.auth.models import Role
+from src.auth.models import Role, User
+
 
 router = APIRouter(
     prefix="/auth",
-    tags=["Auth"],
+    tags=["Authentication"],
 )
 
 
-@router.post("/role")
+@router.post("/role", response_model=RoleCreate)
 async def create_role(role: RoleCreate, db: Session = Depends(get_async_session)):
     async with db as session:
         new_role = Role(**role.model_dump())
@@ -22,10 +23,19 @@ async def create_role(role: RoleCreate, db: Session = Depends(get_async_session)
         return new_role
 
 
-@router.get("/role")
-async def get_role(db: Session = Depends(get_async_session)):
+@router.get("/role", response_model=list[RoleRead])
+async def get_roles(db: Session = Depends(get_async_session)):
     async with db as session:
         query = select(Role)
+        result = await session.execute(query)
+        roles = result.scalars().all()
+        return roles
+
+
+@router.get("/user", response_model=list[UserRead])
+async def get_user(db: Session = Depends(get_async_session)):
+    async with db as session:
+        query = select(User)
         result = await session.execute(query)
         roles = result.scalars().all()
         return roles

b74e3b87568f8a1610a79d3320d89fa48b32b30c
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-09 01:27:52 +0500

    add testing db variables to config.py

--- src/config.py
+++ src/config.py
@@ -16,3 +16,8 @@ SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 SMTP_HOST = os.getenv("SMTP_HOST")
 SMTP_PORT = os.getenv("SMTP_PORT")
+
+DB_HOST_TEST = os.getenv("DB_HOST_TEST")
+DB_USER_TEST = os.getenv("DB_USER_TEST")
+DB_PASS_TEST = os.getenv("DB_PASS_TEST")
+DB_NAME_TEST = os.getenv("DB_NAME_TEST")

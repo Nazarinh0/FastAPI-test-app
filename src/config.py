0d06993624d3d2c05cd6f0c3b68fddb6f897ea60
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-08 20:54:26 +0500

    add variables to config.py

--- src/config.py
+++ src/config.py
@@ -9,4 +9,10 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT")
+
 SECRET = os.getenv("SECRET")
+
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_PORT = os.getenv("SMTP_PORT")

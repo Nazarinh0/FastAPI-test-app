da44f2a9357401594450f2ba72ffd6ef62608869
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-03-10 17:39:38 +0500

    add custom middleware and CORS settings to main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from contextlib import asynccontextmanager
+import time
 
 from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ValidationException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
@@ -43,6 +45,42 @@ app.include_router(
 app.include_router(router_operation)
 app.include_router(router_auth)
 app.include_router(router_task)
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Authorization",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Credentials",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Methods",
+    ],
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    """
+    Calculate the time it takes to process a request and add it to the response headers.
+    """
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 @app.exception_handler(ValidationException)
